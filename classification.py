659ab37b2761d24b4f522f6d69dd8856c7c9312e
Kimaru Thagana <thagana44@gmail.com>
2017-11-06 11:30:18 +0000

    KNN algorithm

---
+++ classification.py
@@ -0,0 +1,22 @@
+#   K NEAREST NEIGHBOURS
+# CHOSING WHICH CLASS A POINT BELONGS TO BY DETERMINING WHICH POINTS ARE CLOSEST
+#K SHOULD BE ODD TO AVOID SPLIT VOTES
+from sklearn import cross_validation,preprocessing,neighbors
+import numpy as np
+import pandas as pd
+
+dataframe=pd.read_csv('Bcancer.txt')
+dataframe.replace('?',-99999,inplace=True)# This replaces the ? with the value -99999
+# thus making the data point an outlier instead of discarding the data point all at once.
+dataframe.drop(['id'],1,inplace=True)
+X=np.array(dataframe.drop(['class'],1))# all the rest are our features
+# their values are use to determine whether a tumor is benign or malignant
+y=np.array(dataframe['class']) #our label
+X_train,X_test,y_train,y_test=cross_validation.train_test_split(X,y,test_size=0.75)
+#use a % of tha data for training
+clf=neighbors.KNeighborsClassifier()
+clf.fit(X_train,y_train)
+accuracy=clf.score(X_test,y_test)
+print(accuracy)
+example_measures=np.array([[10,2,1,1,10,5,3,2,1],[1,6,1,1,1,2,3,2,1]])
+print(clf.predict(example_measures.reshape(len(example_measures),-1) ) )
