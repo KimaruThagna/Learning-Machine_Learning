e9fbc43fe398699ddbb85b1fd300037564f9d3ef
Kimaru Thagana <thagana44@gmail.com>
2017-11-22 19:41:39 +0000

    preprocessing and fitment

--- kmeans.py
+++ kmeans.py
@@ -53,4 +53,43 @@ X=np.array([
 # # of the items in the x array X[:,1] returns all the elements in pos 1 of te items in the X array
 # plt.show()
 df=pd.read_excel('titanic.xls',sheetname='titanic3')
+
+df.drop(['body','name'],1,inplace=True)
+df.convert_objects(convert_numeric=True)# convert df to numeric
+df.fillna(0,inplace=True) # file NaN values with 0s
 print(df.head())
+
+# non numeric data needs to be mapped to a numeric equivalent for ML algorithms to process
+def convert_non_numeric_data(df):
+    columns=df.columns.values # obtain coulmn headers
+    for col in columns:
+        text_digit_vals={} #structure will be {"txt":digit,"txt":digit}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+        if df[col].dtype!=np.int64 and df[col].dtype!=np.float64:
+            contents=df[col].values.tolist() # convert column contents to list
+            unique_elements=set(contents) #this finds non repeating elements
+            x=0
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique]=x
+                    x+=1
+            df[col]=list(map(convert_to_int,df[col]))# maps the dictionary
+            #retuned by the function to the column in the data frame
+    return df
+df=convert_non_numeric_data(df)
+#print(df.head())
+X=np.array(df.drop(['survived'],1).astype(float))
+X=preprocessing.scale(X)# scale the values so that they dont distort the outcome
+# the feature set is everything apart from the survived column
+y=np.array(df['survived'])# labels
+clf=KMeans(n_clusters=2)
+clf.fit(X)
+correct=0
+for i in range(len(X)):
+    predict_me=np.array(X[i].astype(float)) # particular record in scaled df
+    predict_me=predict_me.reshape(-1,len(predict_me)) #reshape record
+    prediction=clf.predict(predict_me) # predict class of record from the trained clf
+    if prediction[0]==y[i]: # compare prediction with survival index, 0 or 1
+        correct+=1
+print(correct/len(X))
