bb242475e7ecd1b1b5e5e4f240e22bec264f5d48
Kimaru Thagana <thagana44@gmail.com>
2018-11-06 04:55:12 -0300

    features selection rfe and extra trees classifier

---
+++ feature_selection.py
@@ -0,0 +1,22 @@
+# Recursive Feature Elimination
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import ExtraTreesClassifier
+# load the iris datasets
+dataset = datasets.load_iris()
+# create a base classifier used to evaluate a subset of attributes
+model = LogisticRegression()
+# create the RFE model and select 3 attributes
+rfe = RFE(model, 3)
+rfe = rfe.fit(dataset.data, dataset.target)
+# summarize the selection of the attributes
+print(rfe.support_)
+print(rfe.ranking_)
+
+# displaying relative feature importance
+# fit an Extra Trees model to the data
+model = ExtraTreesClassifier()
+model.fit(dataset.data, dataset.target)
+# display the relative importance of each attribute
+print(model.feature_importances_)
