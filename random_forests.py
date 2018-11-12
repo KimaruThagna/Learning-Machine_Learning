fca37fc2797edd60ec993fbdf13ddf3a41e9f895
Kimaru Thagana <thagana44@gmail.com>
2018-11-12 05:11:14 -0300

    view single image

--- random_forests.py
+++ random_forests.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
+from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
+
 
 train_data=pd.read_csv('digits_dataset/train.csv')
 test_data=pd.read_csv('digits_dataset/test.csv')
+print(train_data.head())
+
+#inspcet single row
+val=train_data.iloc[0,1: ].values
+val=val.reshape(28,28).astype('uint8')
+plt.imshow(val)
+plt.show()
