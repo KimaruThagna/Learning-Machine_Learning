6f2251baa2b8fd1178fd7edc747162f91461d6e5
Kimaru Thagana <thagana44@gmail.com>
2018-11-13 05:07:02 -0300

    make predictions and print classification report

--- random_forests.py
+++ random_forests.py
@@ -1,6 +1,4 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
@@ -13,3 +11,5 @@ Y=train_data.target
 x_train,y_train,x_test,y_test=train_test_split(X,Y,test_size=0.3,random_state=85)
 clf=RandomForestClassifier(n_estimators=100)# this defines the number of decision trees used
 clf.fit(x_train,y_train)
+predictions=clf.predict(x_test)
+print(classification_report(y_test,predictions))
