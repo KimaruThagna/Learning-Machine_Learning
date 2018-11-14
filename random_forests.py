9de619da2be2ea6f62e8bd547a0135acfce3d8c6
Kimaru Thagana <thagana44@gmail.com>
2018-11-14 04:59:25 -0300

    convert numpy array into pandas dataframe

--- random_forests.py
+++ random_forests.py
@@ -2,12 +2,14 @@ from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+digits=load_digits()
+data=pd.DataFrame(data=np.c_[digits['data'],digits['target']],
+                            columns=digits['feature_names']+['target'])
 
-train_data=load_digits()
-
-print(train_data.target_names)
-X=train_data.data
-Y=train_data.target
+X=data.drop('target',axis=1)
+Y=data['target']
 x_train,y_train,x_test,y_test=train_test_split(X,Y,test_size=0.3,random_state=85)
 clf=RandomForestClassifier(n_estimators=100)# this defines the number of decision trees used
 clf.fit(x_train,y_train)
