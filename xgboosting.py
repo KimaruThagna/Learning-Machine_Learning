2cbbc0b1d76052107ccb4ed81db0133d01818846
Kimaru Thagana <thagana44@gmail.com>
2018-11-17 02:51:58 -0300

    one hot encoding

--- xgboosting.py
+++ xgboosting.py
@@ -11,3 +11,12 @@ from sklearn.model_selection import cross_val_score
 dataset = pd.read_csv('churn_Dataset/Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
