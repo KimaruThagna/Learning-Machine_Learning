50555f4f99294c0c085043e93a6b3e0316be9c71
Kimaru Thagana <thagana44@gmail.com>
2018-11-17 02:57:12 -0300

    print classification report

--- xgboosting.py
+++ xgboosting.py
@@ -34,4 +34,8 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 
 cm = confusion_matrix(y_test, y_pred)
-
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
