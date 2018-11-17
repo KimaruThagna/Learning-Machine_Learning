ee75b503deaf076435139991a8c11b918634bf8a
Kimaru Thagana <thagana44@gmail.com>
2018-11-17 02:55:03 -0300

    predictions

--- xgboosting.py
+++ xgboosting.py
@@ -28,3 +28,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 # Fitting XGBoost to the Training set
 classifier = xgboost.XGBClassifier()
 classifier.fit(X_train, y_train)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
